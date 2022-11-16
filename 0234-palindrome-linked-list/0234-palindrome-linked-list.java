5a0b40adca5f5593b2e992f61e68c678831e208e
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2022-11-17 00:41:46 +0530

    Time: 49 ms (9.57%), Space: 110.8 MB (13.51%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode temp = head;
+        Stack<Integer> s = new Stack<>();
+        while(temp != null) {
+            s.push(temp.val);
+            temp = temp.next;
+        }
+        while (head != null) {
+            if (head.val != s.pop()) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+}
