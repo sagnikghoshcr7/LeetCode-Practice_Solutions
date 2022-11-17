b6255d588e8a4321ed232bf9f73b9c9e7c08595b
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2022-11-17 11:07:38 +0530

    Time: 1 ms (80.80%), Space: 45.4 MB (30.78%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) { // circle detected
+                while (head != fast) {
+                    fast = fast.next;
+                    head = head.next;
+                }
+                return head;
+            }
+        }
+        return null;
+    }
+}
