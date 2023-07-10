b360916bbe708c2315fd1998a17546230ac8fa20
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2023-07-11 01:04:36 +0530

    Time: 34 ms (18.04%), Space: 44.5 MB (63.28%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        for(int i=0; i<names.length; i++){
+            for(int j=1+i; j<names.length; j++){
+                if(heights[i] < heights[j]){
+                    int temp = heights[i];
+                    heights[i] = heights[j];
+                    heights[j] = temp;
+
+                    String temp1 = names[i];
+                        names[i] = names[j];
+                        names[j] = temp1;
+                }
+            }
+        }
+        return names;
+    }
+}
