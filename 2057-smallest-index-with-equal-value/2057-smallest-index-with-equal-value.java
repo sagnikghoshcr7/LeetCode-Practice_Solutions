4ab781f8876fec940bb4d79494d4c5e9c7857887
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2023-07-12 02:21:35 +0530

    Time: 1 ms (100.00%), Space: 43.2 MB (69.94%) - LeetHub

---
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int smallestEqual(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            if (i%10 == nums[i]) return i;
+        }
+        return -1;
+    }
+}
