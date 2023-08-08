024938abd8abfcada5dd00f8b8f0eb1348c55602
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2023-08-08 21:30:04 +0530

    Time: 1 ms (99.6%), Space: 42.5 MB (95.22%) - LeetHub

---
+++ 2916-check-if-it-is-possible-to-split-array/2916-check-if-it-is-possible-to-split-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canSplitArray(List<Integer> nums, int m) {
+        if(nums.size() <= 2) return true;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums.get(i) + nums.get(i-1) >= m) return true;
+        }
+        return false;
+    }
+}
