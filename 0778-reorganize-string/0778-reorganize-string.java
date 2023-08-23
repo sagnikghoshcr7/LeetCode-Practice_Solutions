1f30c5c8efc2c8f83e09d4d34badaa06d1658a5b
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2023-08-23 21:25:05 +0530

    Time: 4 ms (53.93%), Space: 40.6 MB (67.08%) - LeetHub

---
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reorganizeString(String s) {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> freqMap.get(b) - freqMap.get(a));
+        maxHeap.addAll(freqMap.keySet());
+
+        if (freqMap.get(maxHeap.peek()) > (s.length() + 1) / 2) {
+            return "";
+        }
+
+        StringBuilder res = new StringBuilder();
+        char[] result = new char[s.length()];
+        int i = 0;
+        while (!maxHeap.isEmpty()) {
+            char c = maxHeap.poll();
+            for (int j = 0; j < freqMap.get(c); j++) {
+                if (i >= s.length()) i = 1;
+                result[i] = c;
+                i += 2;
+            }
+        }
+
+        return new String(result);
+    }
+}
