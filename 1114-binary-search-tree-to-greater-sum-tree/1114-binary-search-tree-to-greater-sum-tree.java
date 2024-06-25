91968034a5a428b33db1c01475bbd59b3d920d88
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2024-06-25 23:25:36 +0530

    Time: 0 ms (100%), Space: 41.5 MB (9.5%) - LeetHub

---
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,9 @@
+class Solution {
+    int pre = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if (root.right != null) bstToGst(root.right);
+        pre = root.val = pre + root.val;
+        if (root.left != null) bstToGst(root.left);
+        return root;
+    }
+}
