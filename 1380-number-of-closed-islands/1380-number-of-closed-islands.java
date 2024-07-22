3c8c3ea2abe9c040638ce1d0bd2fd92d06ec2548
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2024-07-22 23:52:07 +0530

    Time: 2 ms (100%), Space: 44.6 MB (28.46%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.java
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.java
@@ -2,20 +2,22 @@ class Solution {
     int n, m;
     public int closedIsland(int[][] adj) {
         n = adj.length; m = adj[0].length;
-        int count = 0;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (adj[i][j] == 0 && dfs(adj,i, j)) count++;
+        int cnt = 0;
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (adj[i][j] == 0 && dfs(adj, i, j)) cnt++;
             }
         }
-        
-        return count;
+
+        return cnt;
     }
-    public boolean dfs(int[][] adj,int i, int j) {
-        if (i < 0 || j < 0 || i >= n || j >= m) return false;
+
+    private boolean dfs(int[][] adj, int i, int j) {
+        if (i<0 || j<0 || i>=n || j>=m) return false;
         if (adj[i][j] == 1) return true;
-        adj[i][j] = 1; // mark as visited
+        adj[i][j] = 1;
+
         boolean left = dfs(adj,i, j-1), right = dfs(adj,i, j+1);
         boolean up = dfs(adj,i-1, j), down = dfs(adj,i+1, j);
         return left && right && up && down;
