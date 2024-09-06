0799a26d39344155687f46efe51366c5194b5708
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2024-09-06 23:00:57 +0530

    Time: 3 ms (100%), Space: 60 MB (99.97%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        int max = -1;
+        for(int num : nums ){
+            max = num > max ? num : max;
+        }
+        boolean[] freq = new boolean[max+1];
+
+        for(int num : nums) freq[num] = true;
+
+        ListNode temp = new ListNode();
+        ListNode current = temp;
+
+        while(head != null){
+            if(head.val >= freq.length || freq[head.val] == false){
+                current.next = head;
+                current = current.next;
+            }
+            head = head.next;
+        }
+
+        current.next = null;
+        return temp.next;
+    }
+}
