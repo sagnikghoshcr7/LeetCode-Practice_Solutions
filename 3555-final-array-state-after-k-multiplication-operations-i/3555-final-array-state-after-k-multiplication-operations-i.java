25564c941ce53f291b6a5055f04de9f363c8ad13
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2024-12-17 01:22:51 +0530

    Time: 1 ms (100%), Space: 44.8 MB (51.28%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        int[] arr = new int[2];
+
+        for (int i = 0; i < k; i++) {
+            arr = getMin(nums);
+            nums[arr[1]] = arr[0] * multiplier;
+        }
+
+        return nums;
+    }
+
+    private int[] getMin(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int idx = 0;
+
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] < min) {
+                min = nums[i];
+                idx = i;
+            }
+
+        return new int[] { min, idx };
+    }
+
+}
