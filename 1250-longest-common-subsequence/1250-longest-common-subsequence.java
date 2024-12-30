fabe9f0fbfb80061cafdc879dff9cf5294ab3e2f
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2024-12-30 18:42:46 +0530

    Time: 20 ms (76.97%), Space: 51.1 MB (33.69%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -8,8 +8,8 @@ class Solution {
             for (int j=0; j<=n; j++) {
                 if (i == 0 || j == 0) dp[i][j] = 0;
                 else {
-                    if (text1.charAt(i-1) != text2.charAt(j-1)) dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                    else dp[i][j] = 1 + dp[i-1][j-1];
+                    if (text1.charAt(i-1) == text2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                    else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
