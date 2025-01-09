d82bcc4806f890dfdc006574dc527c2fd1ac519c
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2025-01-09 11:45:35 +0530

    Time: 0 ms (100%), Space: 45.7 MB (85.8%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,26 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        if (n == 0) return new int[]{-1, -1};
-        return new int[]{first(nums, target), last(nums, target)};
+        return new int[]{firstElement(nums, target), lastElement(nums, target)};
     }
 
-    public int first(int[] a, int target) {
-        int l = 0, r = a.length - 1;
-
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-            if (a[mid] == target && (mid == 0 || a[mid-1] < target)) return mid;
-            else if (a[mid] >= target) r = mid-1;
-            else l = mid+1;
+    public int firstElement(int[] a, int k) {
+        int lo = -1, hi = a.length;
+        while (hi-lo>1) {
+            int mid = lo + (hi-lo)/2;
+            if (a[mid]<k) lo = mid;
+            else hi = mid;
         }
-        return -1;
+        return hi != a.length && a[hi] == k ? hi : -1;
     }
 
-    public int last(int[] a, int target) {
-        int l=0, r = a.length-1;
-
-        while (l <= r) {
-            int mid = l + (r-l)/2;
-            if (a[mid] == target && (mid == a.length-1 || a[mid+1]>target)) return mid;
-            else if (a[mid] <= target) l = mid+1;
-            else r = mid-1;
+    public int lastElement(int[] a, int k) {
+        int lo = -1, hi = a.length;
+        while (hi-lo>1) {
+            int mid = lo + (hi-lo)/2;
+            if (a[mid]<=k) lo = mid;
+            else hi = mid;
         }
-        return -1;
+
+        return lo != -1 && a[lo] == k ? lo : -1;
     }
 }
