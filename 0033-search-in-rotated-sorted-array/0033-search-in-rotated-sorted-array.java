2e919f97cb1254459cd9d3bb36ccfbab3206dc40
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2025-01-12 04:44:32 +0530

    Time: 0 ms (100%), Space: 42.4 MB (26.28%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,23 +1,50 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int l=0, r = nums.length-1;
-        
-        while (l <= r) {
-            int mid = (l + r) >> 1;
-            if (nums[mid] == target) return mid;
-            
-            // nums[left to mid] is sorted
-            if (nums[l] <= nums[mid]) {
-                if (target >= nums[l] && target < nums[mid]) r = mid - 1;
-                else l = mid + 1;
-            }
-            
-            // nums[mid to target] is sorted
-            else {
-                if (target > nums[mid] && target <= nums[r]) l = mid + 1;
-                else r = mid - 1;
-            }
+    public int search(int[] a, int target) {
+        int pivot = findPivot(a);
+
+        // Search in the first part (0 to pivot - 1)
+        int ans = bs(a, 0, pivot - 1, target);
+        if (ans != -1) return ans;
+
+        // Search in the second part (pivot to end)
+        return bs(a, pivot, a.length - 1, target);
+    }
+
+    public int findPivot(int[] a) {
+        int lo = 0, hi = a.length - 1;
+
+        while (hi - lo > 1) {
+            int mid = lo + (hi - lo) / 2;
+
+            // Pivot is in the right part if mid is greater than the last element
+            if (a[mid] > a[hi]) lo = mid;
+            else hi = mid; // Pivot is in the left part
         }
-        return -1;
+
+        // The pivot is the smaller of the two remaining elements
+        return a[lo] > a[hi] ? hi : lo;
+    }
+
+    public int bs(int[] a, int s, int e, int target) {
+        int lo = s - 1, hi = e + 1; // Extend bounds to use `hi - lo > 1`
+
+        while (hi - lo > 1) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (a[mid] < target) lo = mid; // Move right
+            else hi = mid; // Move left
+        }
+
+        // After the loop, check if the target is found
+        return (hi >= 0 && hi < a.length && a[hi] == target) ? hi : -1;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {3, 4, 5, 6, 1, 2};
+        int target = 2;
+
+        System.out.println(sol.search(nums, target)); // Output: 5
     }
 }
