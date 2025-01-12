f0bad597337f5b03f98eafe10f8b2d09e3f631a2
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2025-01-13 03:28:54 +0530

    Time: 1 ms (100%), Space: 46.3 MB (22.07%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,33 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
         
-        int i = nums1.length;
-        int j = nums2.length;
+        // Ensure nums1 is the smaller array for simplicity
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
         
-        int[] arr = new int[i+j];
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2; // Calculate the left partition size
+        int lo = 0, hi = n1;
         
-        // Merge two lists into one array
-        System.arraycopy(nums1, 0, arr, 0, i);
-        System.arraycopy(nums2, 0, arr, i, j);
+        while (lo <= hi) {
+            int mid1 = (lo + hi) / 2;
+            int mid2 = left - mid1;
+            
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE, r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            
+            if (mid1 < n1) r1 = nums1[mid1];
+            if (mid2 < n2) r2 = nums2[mid2];
+            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            
+            if (l1 <= r2 && l2 <= r1) {
+                // The partition is correct, we found the median
+                if (n % 2 == 1) return Math.max(l1, l2);
+                else return ((double)(Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            }
+            else if (l1 > r2) hi = mid1 - 1;
+            else lo = mid1 + 1;
+        }
         
-        // Sort the merged array
-        Arrays.sort(arr);
-        
-        int n = arr.length;
-        
-        if (n%2 == 0) {
-            // If length of array is even
-            int mid = n/2;
-            int a = arr[mid];
-            int b = arr[mid - 1];
-            float testa = a;
-			float testb = b;
-            float ans = (testa+testb)/2;
-            return ans;
-        } else {
-            // If length of array is odd
-            int mid = Math.round(n/2);
-            return arr[mid];
-        }        
+        return -1;
     }
 }
