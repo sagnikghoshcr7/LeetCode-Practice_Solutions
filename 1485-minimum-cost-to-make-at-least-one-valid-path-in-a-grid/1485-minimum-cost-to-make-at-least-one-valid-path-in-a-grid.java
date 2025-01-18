c498d83f91a881c9633df5f791139d97a9af644e
Sagnik Ghosh <sagnikghoshmandra@gmail.com>
2025-01-19 03:43:18 +0530

    Time: 10 ms (99.3%), Space: 45.4 MB (33.92%) - LeetHub

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -0,0 +1,29 @@
+class Solution {
+    int INF = (int) 1e9;
+    int[][] DIR = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int minCost(int[][] grid) {
+        int m = grid.length, n = grid[0].length, cost = 0;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) Arrays.fill(dp[i], INF);
+        Queue<int[]> bfs = new LinkedList<>();
+        dfs(grid, 0, 0, dp, cost, bfs);
+        while (!bfs.isEmpty()) {
+            cost++;
+            for (int size = bfs.size(); size > 0; size--) {
+                int[] top = bfs.poll();
+                int r = top[0], c = top[1];
+                for (int i = 0; i < 4; i++) dfs(grid, r + DIR[i][0], c + DIR[i][1], dp, cost, bfs);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    void dfs(int[][] grid, int r, int c, int[][] dp, int cost, Queue<int[]> bfs) {
+        int m = grid.length; int n = grid[0].length;
+        if (r < 0 || r >= m || c < 0 || c >= n || dp[r][c] != INF) return;
+        dp[r][c] = cost;
+        bfs.offer(new int[]{r, c}); // add to try change direction later
+        int nextDir = grid[r][c] - 1;
+        dfs(grid, r + DIR[nextDir][0], c + DIR[nextDir][1], dp, cost, bfs);
+    }
+}
